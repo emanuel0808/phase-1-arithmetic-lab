5cfd8e1fed0e55d0cff8ff4fd19ae98e062a6f5b
Emanuel Gaona <gaonaemanuel7@gmail.com>
2023-04-01 03:12:16 -0700

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
 
-// Write your code here
+const random = 10
+
+const num3 = 8
+const num4 = 2
+const mod = num3 / num4
+
+const max = 20
